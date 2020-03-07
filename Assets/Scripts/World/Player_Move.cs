af7002bfa32cf113c09ec76371b1f99bdd11e2fd
Tisa Realnam <redwailordjpn@gmail.com>
2020-03-07 23:22:31 +0000

    - Added art assets, like placeholder tilesets, NPC sprites, etc. - The player exists and can move (SampleScene).

---
+++ Assets/Scripts/World/Player_Move.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+
+    #region Player components
+    [SerializeField]
+    [Tooltip("Speed with which player moves.")]
+    private float spd = 1f;
+    private Vector3 pos;
+
+    /* For animation stuff, later: */
+    private Animator animate;
+    private bool playerMoving;
+
+    /* True if player can move, false otherwise
+     (mainly a to freeze the player during dialogue events) */
+    public bool canMove;
+
+    private Rigidbody2D rb2d;
+    #endregion
+
+
+    #region Unity fxns
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove) {
+            move();
+        }
+        
+
+    }
+    #endregion
+
+    #region Player movement functions
+    void move() {
+
+        pos = transform.position;
+
+        //update position
+        pos.y += Input.GetAxisRaw("Vertical") * spd * Time.deltaTime;
+        pos.x += Input.GetAxisRaw("Horizontal") * spd * Time.deltaTime;
+
+        //update position in gameobject's transform object
+        transform.position = pos;
+    }
+    #endregion
+
+    #region Player-World Interaction functions
+    /* such as: checking objects */
+
+    #endregion
+}
